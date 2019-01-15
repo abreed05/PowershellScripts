19c79b77f0bc8befaf970156fa79520a65336d97
abreed05 <39411883+abreed05@users.noreply.github.com>
2019-01-14 21:42:58 -0500

    Create Snapshot.ps1

---
+++ Snapshot.ps1
@@ -0,0 +1,29 @@
+# Name: Snapshot
+# Author: Aaron Breeden
+# Purpose: Check if specific snapshot exists and if not create it 
+
+# Variables that can be changed
+$snapshot = 'GOLDEN_SNAPSHOT'
+$desc = "GOLDEN_SNAPSHOT CREATED ON $(Get-Date -Format 'u')"
+
+# ---- DO NOT CHANGE VARIABLES BELOW THIS LINE ----
+
+# Connect to host
+$vmhost = Read-Host -Prompt 'Input IP or FQDN of host'
+$user = Read-Host -Prompt 'Input Username'
+$passwd = Read-Host -Prompt 'Input Password'
+
+Connect-VIServer $vmhost -Protocol https -User $user -Password $passwd
+
+# Get VMs
+$vms = Get-VM
+
+# Loop through each VM, Check if Snapshot exists and if not create it 
+ForEach ($vm in $vms) {
+$snapshotExists = $null
+$snapshotExists = $vm | Get-Snapshot -Name $snapshot -ea SilentlyContinue
+
+# If else statement 
+If ($snapshotExists -eq $null) {$vm | New-Snapshot -Name $snapshot -Description $desc}
+Else {Write-Host "$vm snapshot $snapshot already exists "}
+}
