a9a19fe3b677b1f8cc4ea17c563bad9803962cc4
Aaron Breeden <39411883+abreed05@users.noreply.github.com>
2019-06-17 10:21:31 -0400

    Update Network_Troubleshooting.ps1

--- Network_Troubleshooting.ps1
+++ Network_Troubleshooting.ps1
@@ -3,12 +3,13 @@
 # Purpose: A simple script to test various network functionality including LAN, WAN, internal and public DNS. 
 #          This script will also attempt to fix issues it finds and logs a test results file to the users desktop. 
 
-
 # Variables 
 $_host1 = "IP.IntenralDNS.Server."
 $_host2 = "Ip.Seconday.DNS.Server"
 $_host3 = "FQDN.Somehost.local"
 
+$filePath = "$env:USERPROFILE\Desktop\TestResults.txt"
+$date = Get-Date
 # Functions 
 
 # Function To Test Gateway
@@ -32,42 +33,44 @@ Function Reset-NetworkAdapter {Disable-NetAdapter -Name Wi-Fi -Confirm:$false
                                Netsh Winsock Reset
                                Enable-NetAdapter -Name Wi-Fi -Confirm:$false }
 
+Add-Content -Path $filePath "Starting tests at $date "
+
 # Tests for an APIPA address. If APIPA is confirmed will attempt to release / renew DHCP using the Reset-IP function
 $ipv4 = Test-Connection -ComputerName 172.16.100.7 -Count 1 | select -ExpandProperty IPv4Address
 if ($ipv4.IPAddressToString -Like "*169.254*" ) { 
                                                  Write-host "BAD IP"
                                                  write-host "Attempting to renew IP Address "
                                                  Reset-IP
-                                                 Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "APIPA Address fonund Attempted fix" }
+                                                 Add-Content -path $filePath "`nAPIPA Address fonund Attempted fix" }
 Else {Write-Host "Getting an expected IP"
-      Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "Getting an expected IP address"
+      Add-Content -path $filePath "`nGetting an expected IP address"
       }
 
 # Ping Gateway
 If (Test-Gateway -eq true) {
     write-host "Gateway pings successful"
-    Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "`nGateway test passed"
+    Add-Content -path $filePath "`nGateway test passed"
 }
 
 Else {write-host "Gateway test failed"
-      Add-Content -path $env:USERPROFILEDesktop\TestResults.Txt "`nGateway test failed"}
+      Add-Content -path $filePath "`nGateway test failed"}
 
 # Test Lan Connectivity 
 
 If (Test-Lan -eq true) {
     write-host "Lan test successful"
-    Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "`nLan test passed"
+    Add-Content -path $filePath "`nLan test passed"
 }
 
 Else {write-host "Lan test failed"
-      Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "`nLAN test failed"
+      Add-Content -path $filePath "`nLAN test failed"
       
 }
 
 # Perfrom wan test 
 If (Test-Wan -eq true) {
     Write-host "You have wan connectivity" 
-    Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "`nWAN test passed"
+    Add-Content -path $filePath "`nWAN test passed"
 }
 Else {write-host "WAN test failed"
       Reset-IP
@@ -75,17 +78,18 @@ Else {write-host "WAN test failed"
       Test-Gateway
       Test-Lan
       Test-Wan
-      Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "`nWan test failed, attempted to reset IP and Network adapter"}
+      Add-Content -path $filePath "`nWan test failed, attempted to reset IP and Network adapter"}
 
 # Perform public DNS test
 If (Test-DNS -eq true) {
     Write-host "DNS is resolving as expected"
-    Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "`nPublic DNS test passed" 
+    Add-Content -path $filePath "`nPublic DNS test passed" 
 }
 Else {write-host "DNS test failed, flushing DNS"
       ipconfig /flushdns
       Test-DNS
-      Add-Content -path $env:USERPROFILE\Desktop\TestResults.Txt "`nPublic DNS test failed. Flushed DNS cache"}
+      Add-Content -path $filePath "`nPublic DNS test failed. Flushed DNS cache"}
+
 
 
 
