c30fe97089e6201a9c6a4760480671048648c1b3
Aaron Breeden <39411883+abreed05@users.noreply.github.com>
2020-05-15 13:09:13 -0400

    Update Remove-SignatureFolder.ps1

--- Remove-SignatureFolder.ps1
+++ Remove-SignatureFolder.ps1
@@ -1,13 +1,17 @@
+# Name: Remove-SignatureFile
+# Author: Aaron Breeden
+# Purpose: Removes the Signatures folder within the Microsoft Outlook Client
+
 # enumerate all user profiles within C:\users
 $users = @(Get-ChildItem 'HKLM:\Software\Microsoft\Windows NT\currentVersion\ProfileList' |% {Get-ItemProperty $_.pspath})
 
-# for each loop to loop through each folder found in C:\users
+# Loops through each user found on the machine 
 foreach($user in $users.profileImagePath) { # begin foreach loop
 
 # test to check if folder exists
 $test = test-path -path "$user\appdata\Roaming\Microsoft\Signatures" 
 
-    # if folder exsists remove all items from within the Signature folder
+    # if folder exsists remove all items from within the Signatures folder
     if ($test -eq "True") { #begin if statement
         Remove-Item -Path $user\appdata\Roaming\Microsoft\Signatures\* -Recurse -Force
     } # end if statement
